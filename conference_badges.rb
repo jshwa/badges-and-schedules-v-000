b49379da30c7601c33ef4c5da8e593456f66f45d
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-02 18:54:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my names is #{name}"
+end 
+
+def batch_badge_creator(roster)
+  badge_roster = []
+  roster.each do |name|
+    badge_roster.push(badge_maker(name))
+  end
+  badge_roster
+end
+
+def assign_rooms(roster)
+  room_list = []
+  roster.each_with_index do |name, room|
+    room_list.push("Hello, #{name}! You have been assigned to room #{room}!")
+  end
+  room_list
+end
+
+def printer(roster)
+  batch_badge_creator(roster).each do |i|
+    puts i 
+  end
+  assign_rooms(roster.each) do |i|
+    puts i 
+  end
+end
